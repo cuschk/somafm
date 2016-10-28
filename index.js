eab7950470a5bbf2e1c0cd62c3bc6d192a70fe09
Christoph Uschkrat <christoph@uschkrat.com>
2016-10-28 21:01:48 +0200

    Cache channels for a minute

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const got = require('got');
 const trim = require('trim');
+const CacheConf = require('cache-conf');
 const pkg = require('./package.json');
 
 const PREFERRED_STREAMS = [
@@ -18,9 +19,12 @@ const GOT_OPTS = {
   }
 };
 
-const somafm = {};
+const channelsConf = new CacheConf({projectName: pkg.name, configName: 'channels'});
 
-somafm.getChannels = (options, cb) => {
+const somafm = {};
+let channels;
+
+function getChannels(options, cb) {
   if (typeof options === 'function') {
     cb = options;
     options = {};
@@ -29,97 +33,120 @@ somafm.getChannels = (options, cb) => {
   }
 
   options = Object.assign({}, options);
-
   options.streams = Object.assign(PREFERRED_STREAMS, options.streams);
 
-  got('https://api.somafm.com/channels.json', GOT_OPTS)
-    .then(res => {
-      parse(res.body, options, cb);
-    })
-    .catch(err => {
-      cb(err);
-    });
+  channels = readChannels();
 
-  function parse(res, options, cb) {
-    res = JSON.parse(res);
+  if (channels.length === 0 || options.forceUpdate) {
+    got('https://api.somafm.com/channels.json', GOT_OPTS)
+      .then(res => {
+        parseData(res.body, options, (err, res) => {
+          if (err) {
+            cb(err);
+            return;
+          }
 
-    const data = options.sortChannels ?
-      res.channels.sort(compareChannelObjects) :
-      res.channels;
-    let channels = [];
+          writeChannels(res);
 
-    if (options.raw) {
-      return cb(null, data);
-    }
+          cb(null, res);
+        });
+      })
+      .catch(err => {
+        cb(err);
+      });
+  } else {
+    cb(null, channels);
+  }
+}
 
-    data.forEach(channel => {
-      const streamHighestQuality = getHighestQualityStream(channel);
+function parseData(res, options, cb) {
+  res = JSON.parse(res);
 
-      const channelObj = {
-        id: channel.id,
-        title: channel.title,
-        fullTitle: `SomaFM ${channel.title}`,
-        description: trim(channel.description),
-        dj: channel.dj,
-        genre: channel.genre.replace(/\|/g, '/'),
-        lastPlaying: channel.lastPlaying,
-        listeners: channel.listeners,
-        stream: streamHighestQuality
-      };
-      channels.push(channelObj);
-    });
+  const data = options.sortChannels ?
+    res.channels.sort(compareChannelObjects) :
+    res.channels;
+  let channels = [];
 
-    if (options.search && options.search.length > 0) {
-      channels = filterChannels(channels, options.search);
-    }
-
-    return cb(null, channels);
+  if (options.raw) {
+    return cb(null, data);
   }
 
-  function compareChannelObjects(a, b) {
-    return a.title.toLowerCase().localeCompare(b.title.toLowerCase());
+  data.forEach(channel => {
+    const streamHighestQuality = getHighestQualityStream(channel, options.streams);
+
+    const channelObj = {
+      id: channel.id,
+      title: channel.title,
+      fullTitle: `SomaFM ${channel.title}`,
+      description: trim(channel.description),
+      dj: channel.dj,
+      genre: channel.genre.replace(/\|/g, '/'),
+      lastPlaying: channel.lastPlaying,
+      listeners: channel.listeners,
+      stream: streamHighestQuality
+    };
+    channels.push(channelObj);
+  });
+
+  if (options.search && options.search.length > 0) {
+    channels = filterChannels(channels, options.search);
   }
 
-  function getHighestQualityStream(channel) {
-    for (let i = 0; i < options.streams.length; i++) {
-      const stream = options.streams[i];
+  return cb(null, channels);
+}
 
-      for (let j = 0; j < channel.playlists.length; j++) {
-        if (channel.playlists[j].quality === stream.quality && channel.playlists[j].format === stream.format) {
-          const res = {
-            url: channel.playlists[j].url,
-            format: channel.playlists[j].format,
-            quality: channel.playlists[j].quality
-          };
+function compareChannelObjects(a, b) {
+  return a.title.toLowerCase().localeCompare(b.title.toLowerCase());
+}
 
-          return res;
-        }
+function getHighestQualityStream(channel, streams) {
+  for (let i = 0; i < streams.length; i++) {
+    const stream = streams[i];
+
+    for (let j = 0; j < channel.playlists.length; j++) {
+      if (channel.playlists[j].quality === stream.quality && channel.playlists[j].format === stream.format) {
+        const res = {
+          url: channel.playlists[j].url,
+          format: channel.playlists[j].format,
+          quality: channel.playlists[j].quality
+        };
+
+        return res;
+      }
+    }
+  }
+
+  return null;
+}
+
+function filterChannels(channels, keywords) {
+  const regexes = [];
+  for (let i = 0; i < keywords.length; i++) {
+    regexes.push(new RegExp(keywords[i]));
+  }
+
+  return channels.filter(channel => {
+    for (let i = 0; i < regexes.length; i++) {
+      if (!channel.title.match(regexes[i]) && !channel.description.match(regexes[i]) && !channel.genre.match(regexes[i]) && !channel.dj.match(regexes[i])) {
+        return false;
       }
     }
 
-    return null;
+    return true;
+  });
+}
+
+function getChannel(id, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  } else if (!options) {
+    options = {};
   }
 
-  function filterChannels(channels, keywords) {
-    const regexes = [];
-    for (let i = 0; i < keywords.length; i++) {
-      regexes.push(new RegExp(keywords[i]));
-    }
+  options = Object.assign({sortChannels: true}, options);
 
-    return channels.filter(channel => {
-      for (let i = 0; i < regexes.length; i++) {
-        if (!channel.title.match(regexes[i]) && !channel.description.match(regexes[i]) && !channel.genre.match(regexes[i]) && !channel.dj.match(regexes[i])) {
-          return false;
-        }
-      }
-
-      return true;
-    });
-  }
-};
-
-somafm.getChannel = (id, cb) => {
-  somafm.getChannels({sortChannels: true}, (err, res) => {
+  getChannels(options, (err, res) => {
     if (err) {
       return cb(err, null);
     }
@@ -132,6 +159,18 @@ somafm.getChannel = (id, cb) => {
 
     return cb(new Error('Channel not found.'), null);
   });
-};
+}
+
+function readChannels() {
+  return channelsConf.get('channels') || [];
+}
+
+function writeChannels(channels) {
+  // cache channels for one minute
+  channelsConf.set('channels', channels, {maxAge: 60000});
+}
+
+somafm.getChannels = getChannels;
+somafm.getChannel = getChannel;
 
 module.exports = somafm;
